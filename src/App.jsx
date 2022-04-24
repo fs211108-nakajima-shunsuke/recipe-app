8705cf642ce661116fa24f57c9a36969cef18957
Nakajima <s_nakajima@freestyles.jp>
2022-04-24 22:22:39 +0900

    レシピアプリ作成開始 検索窓作成 APIキー用のconfigファイル作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [search, setSearch] = useState("");
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
+
   return (
     <div className="App">
-      <h1>Reactのひな型</h1>
+      <form>
+        <input type="text" value={search} onChange={updateSearch} />
+        <button type="submit">検索</button>
+      </form>
     </div>
   );
 }
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const apiConfig = {
+    appId = "803c357a",
+    apiKey = "fdfb7e8c17a40ce5c3f67ed4e889541e"
+};
